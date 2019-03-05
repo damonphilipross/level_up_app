ea834ee6284849f0950ac3ddc3298bf9db3698b2
Damon Ross <damonphilipross@gmail.com>
2019-03-05 14:18:28 +1100

    validations for models

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,2 +1,3 @@
 class Badge < ApplicationRecord
+  has_many :participants, through: :participant_badges
 end
--- app/models/daily_goal_task.rb
+++ app/models/daily_goal_task.rb
@@ -1,3 +1,5 @@
 class DailyGoalTask < ApplicationRecord
   belongs_to :daily_goal
+  has_many :participant_photos
+
 end
