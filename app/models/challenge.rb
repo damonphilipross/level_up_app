142d8fea715bc24990a95990de1851b220ed5e8b
Damon Ross <damonphilipross@gmail.com>
2019-03-05 14:40:25 +1100

    added in new title column to challenge

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -3,5 +3,7 @@ class Challenge < ApplicationRecord
   has_many :daily_goal
 
   validates :start_date, presence: true
+  validates :description, presence: true
+  validates :title, presence: true
   validates :duration_days, numericality: { only_integer: true, greater_than: 0 }
 end
---
+++ db/migrate/20190305033444_add_title_column_to_challenges.rb
@@ -0,0 +1,5 @@
+class AddTitleColumnToChallenges < ActiveRecord::Migration[5.2]
+  def change
+    add_column :challenges, :title, :string
+  end
+end
