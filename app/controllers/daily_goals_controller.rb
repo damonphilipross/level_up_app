c59883d0e85c8a20acc2361afc356d5026d98d6d
dale hindle <dale.hindle@yahoo.ca>
2019-03-07 13:50:58 +1100

    daily_goals form working

--- app/controllers/daily_goals_controller.rb
+++ app/controllers/daily_goals_controller.rb
@@ -1,2 +1,10 @@
 class DailyGoalsController < ApplicationController
+
+  def new
+    @challenge = Challenge.find(params[:challenge_id])
+  end
+
+  def create
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :participants, only: [ :create, :show, :index, :new]
     # after creating a challenge we redirect to the index of the daily goals
     # Here we can select the a daily goal, go the the show page and add daily goal tasks
-    resources :daily_goal, only: [ :index, :new, :create ]
+    resources :daily_goals, only: [ :index, :new, :create ]
   end
   resources :daily_goals, only: [ :show ] do
     resources :daily_goals_tasks, only: [ :edit, :update, :destroy, :new, :create ]
