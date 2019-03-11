edf7f699911a3fbb2004d8042ec4c3460d43bb9f
dale hindle <dale.hindle@yahoo.ca>
2019-03-11 14:39:50 +1100

    new edits git status

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,10 @@
 import "bootstrap";
 import { clickNext1, clickNext2 } from '../components/challenge_form';
 import { headerCallback } from '../components/challenge_calendar';
+import { collapse } from '../components/challenge_collapse';
 clickNext1();
 clickNext2();
-<<<<<<< HEAD
 headerCallback();
-=======
-import { collapse } from '../components/challenge_collapse';
+
+
 collapse();
->>>>>>> master
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,18 @@
+
 Rails.application.routes.draw do
 
-  devise_for :users
-  root to: 'pages#home'
-  resources :challenges, only: [ :create, :edit, :index, :new ] do
-    resources :participants, only: [ :create, :show, :index, :new ]
-    post 'participants/upvote', to: 'participants#upvote', as: :upvotr
-    # after creating a challenge we redirect to the index of the daily goals
-    # Here we can select the a daily goal, go the the show page and add daily goal tasks
-<<<<<<< HEAD
-    resources :daily_goals, only: [ :index, :new, :create, :edit, :update, :destroy ]
-  end
-  resources :daily_goals, only: [ :show, :edit ] do
-    resources :daily_goals_tasks, only: [ :edit, :update, :destroy, :new, :create ]
-=======
-    resources :daily_goals, only: [ :index, :new, :create, :show ] do
-      resources :daily_goals_tasks, only: [ :index ] do
-        resources :participant_photos, only: [ :index, :new ]
-      end
-    end
-  end
-  resources :daily_goals, only: [ :show ] do
-    resources :daily_goals_tasks, only: [ :edit, :update, :destroy, :new, :create, :index ]
->>>>>>> master
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ devise_for :users
+ root to: 'pages#home'
+ resources :challenges, only: [ :create, :edit, :index, :new ] do
+   resources :participants, only: [ :create, :show, :index, :new ]
+   post 'participants/upvote', to: 'participants#upvote', as: :upvotr
+   # after creating a challenge we redirect to the index of the daily goals
+   # Here we can select the a daily goal, go the the show page and add daily goal tasks
+   resources :task_results, only: [ :index, :edit, :update ]
+   resources :daily_goals, only: [ :index, :new, :create, :edit, :update, :destroy ]
+ end
+ resources :daily_goals, only: [ :show ] do
+   resources :daily_goals_tasks, only: [ :edit, :update, :destroy, :new, :create, :index ]
+ end
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
