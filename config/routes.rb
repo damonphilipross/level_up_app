521387e3959fae7ec2b194cdd5832806d9a95e2d
Damon Ross <damonphilipross@gmail.com>
2019-03-11 14:01:41 +1100

    created task controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'task_results/index'
+  get 'daily_goal_tasks/index'
+  get 'daily_goal_task/index'
   devise_for :users
   root to: 'pages#home'
   resources :challenges, only: [ :create, :edit, :index, :new ] do
@@ -7,11 +10,8 @@ Rails.application.routes.draw do
     post 'participants/upvote', to: 'participants#upvote', as: :upvotr
     # after creating a challenge we redirect to the index of the daily goals
     # Here we can select the a daily goal, go the the show page and add daily goal tasks
-    resources :daily_goals, only: [ :index, :new, :create, :show ] do
-      resources :daily_goals_tasks, only: [ :index ] do
-        resources :participant_photos, only: [ :index, :new ]
-      end
-    end
+    resources :daily_goals, only: [ :index, :new, :create, :show ]
+    resources :task_results, only: [ :index, :edit, :update ]
   end
   resources :daily_goals, only: [ :show ] do
     resources :daily_goals_tasks, only: [ :edit, :update, :destroy, :new, :create, :index ]
