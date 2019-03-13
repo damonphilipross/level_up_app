e134be8f9367954f115781cb7d41b8569217942e
dale hindle <dale.hindle@yahoo.ca>
2019-03-13 17:42:35 +1100

    migration for avatar photo in challenges table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_10_223745) do
+ActiveRecord::Schema.define(version: 2019_03_13_063914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2019_03_10_223745) do
     t.string "title"
     t.text "description"
     t.string "photo"
+    t.string "avatar_photo"
     t.index ["user_id"], name: "index_challenges_on_user_id"
   end
 
@@ -111,9 +112,9 @@ ActiveRecord::Schema.define(version: 2019_03_10_223745) do
     t.bigint "daily_goal_task_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "entry"
     t.string "photo"
     t.text "diary_entry"
+    t.string "entry"
     t.index ["daily_goal_task_id"], name: "index_participant_photos_on_daily_goal_task_id"
     t.index ["participant_id"], name: "index_participant_photos_on_participant_id"
   end
